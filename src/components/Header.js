e2a66d53a3016a5dd4f91c913260f83903418912
WalterJin2 <walterjin2@gmail.com>
2023-08-31 11:02:11 +1000

    啊

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,20 @@ import React, { useState, useEffect, useRef } from 'react'
 
 const Header = () => {
 
-    <div className='h-3'>
-    
-    </div>
+    <header className="bg-blue-500 p-4 flex justify-between items-center">
+        {/* Logo */}
+        <div className="text-white font-bold text-xl">Logo</div>
+
+        {/* Navigation Buttons */}
+        <nav className="space-x-4">
+            <a href="#" className="text-white hover:underline">Job Search</a>
+            <a href="#" className="text-white hover:underline">Find Employers</a>
+            <a href="#" className="text-white hover:underline">Career Advice</a>
+            <a href="#" className="text-white hover:underline">Recruiters</a>
+            <a href="#" className="text-white hover:underline">Sign In</a>
+            <a href="#" className="text-white hover:underline">My Account</a>
+        </nav>
+    </header>
   
 }
 export default Header
