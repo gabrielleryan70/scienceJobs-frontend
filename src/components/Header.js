34d107a5ca76f4730a921e878ab7e3c69d8a4a7f
WalterJin2 <walterjin2@gmail.com>
2023-09-06 16:18:12 +1000

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react'
-
-
 const Header = () => {
     return (
-    <div className="w-full bg-blue-500 p-4 flex justify-start items-center">
-    
-        <div className="text-white font-bold text-xl w-[300px]">Logo</div>
-
-   
-            <nav className="space-x-4 flex justify-around items-center">
-            <a href="#" className="text-white hover:underline">Job Search</a>
-            <a href="#" className="text-white hover:underline">Find Employers</a>
-            <a href="#" className="text-white hover:underline">Career Advice</a>
-            <a href="#" className="text-white hover:underline">Recruiters</a>
-            <a href="#" className="text-white hover:underline">Sign In</a>
-            <a href="#" className="text-white hover:underline">My Account</a>
-        </nav>
+        <div className="  w-full  p-4 flex justify-evenly items-center ">
+            <a href="#" className="text-gray-900 hover:underline">Job Search</a>
+            <a href="#" className="text-gray-900 hover:underline">Find Employers</a>
+            <a href="#" className="text-gray-900 hover:underline">Career Advice</a>
+            <a href="#" className="text-gray-900 hover:underline">Recruiters</a>
+            <a href="#" className="text-gray-900 hover:underline">Sign In</a>
+            <a href="#" className="text-gray-900 hover:underline">My Account</a>
         </div>
     )
-  
 }
 export default Header
--- src/views/Home.js
+++ src/views/Home.js
@@ -4,80 +4,67 @@ import { useNavigate } from "react-router-dom"
 import { Link } from 'react-router-dom'
 import JobDetail from './JobDetail'
 import SingleQA from './SingleQA'
-// import ReactPlayer from 'react-player/lazy'
-const countryMappings = {
-  australia: 'australia',
-  asia: 'asia',
-  africa: 'africa',
-  'new zealand': 'newzealand',
-  canada: 'canada',
-  europe: 'europe',
-  india: 'india',
-  global: 'global',
-  'united states': 'usa',
-  'united kingdom': 'uk'
-};
 const Home = () => {
-  const navigate = useNavigate()
   return (
-    <div className='w-full h-full'>
-      <div className="flex justify-between  h-[600px]">
-        <div className="bg-gray-800 w-[40%] relative z-10">
-          <div className="left absolute top-0 left-0 flex flex-col gap-10 p-8 text-white">
-            <div className="caption">
-              <div className="heading text-5xl font-bold">ALL YOUR BEST</div>
-              <div className="heading text-5xl font-bold">UNIVERSITY JOBS</div>
-            </div>
-            <p className="Text__StyledText-sc-19efksu-0 pages__Description-e0fmos-2 dGrteI">
-              <span className="meet font-bold text-orange">Find the best academic positions and administrative jobs in higher ed. </span>
-              Search for academic jobs, college careers, and faculty positions online. Jobs in higher ed, research, and science.
-            </p>
-            <div className="button-list flex">
-              <button id="redirect" className="button fRVdno"
-                onClick={() => {
-                  const fetchLocation1 = async () => {
-                    try {
-                      const response = await fetch("https://api.geoapify.com/v1/ipinfo?apiKey=ea0e191c22a94bf39e0e58ffbe2d6b66");
-                      const result = await response.json();
-                      return result.country.name
-
-                    } catch (error) {
-                      return ""
-                    }
-                  }
-                  fetchLocation1()
-                    .then(country => {
-                      //ar newUrl = `https://academicjobs.com/`
-                      sessionStorage.setItem("location", countryMappings[country.toLowerCase()]);
-                      navigate(`/${countryMappings[country.toLowerCase()]}`)
-                      //window.location.href = newUrl;
-                    })
-                    .catch(error => {
-                      console.log('Error:', error);
-                    });
-
-              }}
-              >
-                Go to your country's Site
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="w-[59%]">
-          {/* <ReactPlayer
-            url='https://player.vimeo.com/video/827136257?h=de24582070&badge=0&autoplay=1&loop=1&autopause=0&background=1&muted=1&poster=https://f000.backblazeb2.com/file/academicjobs1/homeVideoPic.png&player_id=0&app_id=58479'
-            playing // This prop enables autoplay
-            loop
-            muted
-            controls // You can include controls if needed
-            width="100%"
-            height="100%"
-          /> */}
+    <>
+      <div className="bg-white min-h-screen flex flex-col items-center mt-16">
+        <img
+          src="https://academicjobs.s3.amazonaws.com/img/_misc/ScienceJobsLogo.png"
+          alt="Google Logo"
+          className="w-[30rem] "
+        />
+        <div className="flex mt-5 border rounded-lg p-2 shadow-md w-3/4">
+          <input
+            type="text"
+            className="flex-grow px-4 py-2 focus:outline-none"
+            placeholder="Jobs Search"
+          />
+          <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+            Search In Your Country
+          </button>
         </div>
       </div>
-
-      <SingleQA />
-    </div>
+      <div className="bg-white flex flex-col px-[10rem] ">
+        <h2 className="text-xl text-orange-500 font-extrabold">REGION HOME PAGES</h2>
+      
+      <div className="grid grid-cols-4 gap-4 py-5 text-left">
+        <a href="https://academicjobs.com/usa/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold  ">
+          United States
+        </a>
+        <a href="https://academicjobs.com/australia/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">
+          Australia
+        </a>
+        <a href="https://academicjobs.com/uk/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">
+          United Kingdom
+        </a>
+        <a href="https://academicjobs.com/canada/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">
+          Canada
+        </a>
+        <a href="https://academicjobs.com/newzealand/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">New Zealand
+        </a>
+        <a href="https://academicjobs.com/europe/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">
+          Europe
+        </a>
+        <a href="https://academicjobs.com/india/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">
+          India
+        </a>
+        <a href="https://academicjobs.com/asia/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">
+          Asia
+        </a>
+        <a href="https://academicjobs.com/africa/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">Africa
+        </a>
+        <a href="https://academicjobs.com/middleeast/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">
+          Middle East
+        </a>
+        <a href="https://academicjobs.com/southamerica/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">
+          South America
+        </a>
+        <a href="https://academicjobs.com/jobs/other-jobs/" className="text-orange-500 hover:text-orange-600 text-lg font-semibold ">
+          Other
+        </a>
+        </div>
+      </div>
+    </>
   )
 }
 export default Home
