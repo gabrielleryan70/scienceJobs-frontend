9bef4c3c19e98ed868763cdb10fea739db9d9c1a
WalterJin2 <walterjin2@gmail.com>
2023-09-07 12:21:33 +1000

    a

--- src/views/JobList.js
+++ src/views/JobList.js
@@ -2,26 +2,25 @@ import React, { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import {
   useGetJobsQuery,
-
 } from '../store/apiSlice'
 import { setJob, setId, setStatusId } from '../store/postsSlice'
 const JobList = () => {
   console.log('JobList')
-
   const dispatch = useDispatch()
+
   const {
     data,
     isLoading,
     isSuccess,
   } = useGetJobsQuery(useSelector((state) => state.posts.searchJobCriteria))
-
+  useEffect(() => {
+    if(data)   dispatch(setJob(data[0]));
+  }, [data, dispatch]);
   let content
-
   if (isLoading) {
     content = <p>Loading...</p>
   } else if (isSuccess) {
     console.log(data)
-
     content = data.map((job, index) => {
       const { id, title } = job;
       return (
@@ -34,9 +33,8 @@ const JobList = () => {
         </div>
       );
     });
-    dispatch(setJob(data[0]))
+   // dispatch(setJob(data[0]))
   }
   return <div className='overflow-y'>{content}</div>
 }
-
 export default JobList
