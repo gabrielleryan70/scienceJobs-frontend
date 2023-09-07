f8b8ebe5d46584899d2b967333f7538c1779e929
WalterJin2 <walterjin2@gmail.com>
2023-09-07 14:44:15 +1000

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect, useRef } from 'react'
 const Header = () => {
     return (
         <div className="  w-full  flex justify-between items-center ">
-            <a href="#" className="text-gray-900 hover:underline">Job Search</a>
-            <a href="#" className="text-gray-900 hover:underline">Find Employers</a>
-            <a href="#" className="text-gray-900 hover:underline">Career Advice</a>
-            <a href="#" className="text-gray-900 hover:underline">Recruiters</a>
-            <a href="#" className="text-gray-900 hover:underline">Sign In</a>
-            <a href="#" className="text-gray-900 hover:underline">My Account</a>
+            <div className=" flex justify-between  gap-10">
+                <a href="#" className="text-gray-900 hover:underline">Job Search</a>
+                <a href="#" className="text-gray-900 hover:underline">Find Employers</a>
+                <a href="#" className="text-gray-900 hover:underline">Career Advice</a>
+                
+            </div>
+            <div className=' flex justify-between gap-10'>
+                <a href="#" className="text-gray-900 hover:underline">Sign In</a>
+                <a href="#" className="text-gray-900 hover:underline">My Account</a>
+                <a href="#" className="text-orange-600 hover:underline font-bold">Recruiters</a>
+            </div>
         </div>
     )
 }
