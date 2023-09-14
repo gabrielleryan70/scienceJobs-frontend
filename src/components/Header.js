d40b80bd5455a6a228a13e065dd796e946209c31
WalterJin2 <walterjin2@gmail.com>
2023-09-14 10:41:11 +1000

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,13 @@ const Header = () => {
     return (
         <div className="  w-full  flex justify-between items-center ">
             <div className=" flex justify-between  gap-10">
+                <img
+                    className='w-20'
+                    src="https://academicjobs.s3.amazonaws.com/icon/ACjobsIcon-min.png" alt="" />
                 <a href="#" className="text-gray-900 hover:underline">Job Search</a>
                 <a href="#" className="text-gray-900 hover:underline">Find Employers</a>
                 <a href="#" className="text-gray-900 hover:underline">Career Advice</a>
+                <a href="https://academicjobs.com/au-post-a-job/" className="text-gray-900 hover:underline">Post Jobs</a>
 
             </div>
             <div className=' flex justify-between gap-10'>
