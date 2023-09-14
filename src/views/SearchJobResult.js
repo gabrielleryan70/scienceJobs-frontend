392c0c8a145f11aba69029407a41aa0b30382d8d
WalterJin2 <walterjin2@gmail.com>
2023-09-14 15:55:27 +1000

    a

--- src/views/SearchJobResult.js
+++ src/views/SearchJobResult.js
@@ -40,6 +40,42 @@ const SearchJobResult = () => {
     }
 
   }
-  return <div className='overflow-y w-full'>{content}</div>
+  return <div className='overflow-y w-full'>
+    <div className="bg-gray-100 py-10">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-screen-xl mx-auto">
+          <div className="flex items-center space-x-4">
+            <input
+              type="text"
+              className="w-1/2 px-4 py-2 border border-gray-300 rounded-md focus:ring-orange-500 focus:border-orange-500"
+              placeholder="Keyword"
+              ref={keyWordRef}
+            />
+            <input
+              type="text"
+              className="w-1/2 px-4 py-2 border border-gray-300 rounded-md focus:ring-orange-500 focus:border-orange-500"
+              placeholder="Location"
+              ref={locationRef}
+            />
+            <button
+              className="bg-orange-500 hover:bg-orange-600 text-white py-2 px-6 rounded-md focus:ring-2 focus:ring-orange-300"
+              onClick={() => {
+                const a = {}
+                if (keyWordRef.current.value.trim()) a.q = keyWordRef.current.value.trim()
+                if (locationRef.current.value.trim()) a.l = locationRef.current.value.trim()
+                console.log(a)
+                dispatch(setSearchJobCriteria(a))
+              }}
+            >
+              FIND JOBS
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    {content}
+  
+  </div>
 }
 export default SearchJobResult
