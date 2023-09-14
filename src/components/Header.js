c86ac729b970152df52c510572a1429a1269fc3b
WalterJin2 <walterjin2@gmail.com>
2023-09-14 11:28:25 +1000

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,19 @@ import React, { useState, useEffect, useRef } from 'react'
 const Header = () => {
     return (
         <div className="  w-full  flex justify-between items-center ">
-            <div className=" flex justify-between  gap-10">
+            <div className=" flex justify-between items-center gap-10">
                 <img
                     className='w-20'
                     src="https://academicjobs.s3.amazonaws.com/icon/ACjobsIcon-min.png" alt="" />
-                <a href="#" className="text-gray-900 hover:underline">Job Search</a>
-                <a href="#" className="text-gray-900 hover:underline">Find Employers</a>
-                <a href="#" className="text-gray-900 hover:underline">Career Advice</a>
-                <a href="https://academicjobs.com/au-post-a-job/" className="text-gray-900 hover:underline">Post Jobs</a>
+                <a href="https://academicjobs.com/jobs/" className="text-gray-900 hover:underline">Job Search</a>
+                <a href="https://academicjobs.com/find-employers/" className="text-gray-900 hover:underline">Find Employers</a>
+                <a href="https://academicjobs.com/career-advice/" className="text-gray-900 hover:underline">Career Advice</a>
+               
 
             </div>
             <div className=' flex justify-between gap-10'>
-                <a href="#" className="text-gray-900 hover:underline">Sign In</a>
-                <a href="#" className="text-gray-900 hover:underline">My Account</a>
-                <a href="#" className="text-[#f4a10c] hover:underline font-bold">Recruiters</a>
+                <a href="#" className="text-[#f4a10c] hover:underline font-bold">Global</a>
+                <a href="https://academicjobs.com/recruitment" className="text-[#f4a10c] hover:underline font-bold">Recruiters</a>
             </div>
         </div>
     )
